26ce3f4bfa32a3fcb42dd67f863914b154b1379c
klimovich_y <klimovich_y@Admin2.brest.local>
2019-06-21 15:00:56 +0300

    Количество цифр в Числе

---
+++ src/dz/Dz.java
@@ -0,0 +1,13 @@
+//Новое изменение
+package dz;
+public class Dz {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Количество цифр в числе
+        int x = 1000000; 
+        int num = (int)Math.log10(x)+1;
+        System.out.println("Colvo " + num);
+
+    }
+}
+

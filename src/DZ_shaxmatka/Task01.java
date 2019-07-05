eadee511f45b2fe8581eac0540fee32731571fc9
klimovich_y <klimovich_y@Admin2.brest.local>
2019-07-05 13:34:16 +0300

    Доделал коза воза ноза

---
+++ src/DZ_shaxmatka/Task01.java
@@ -0,0 +1,14 @@
+package DZ_shaxmatka;
+public class Task01 {
+    public static void main(String arg[]){
+        //System.out.println(Task01.mas.lenght);
+       //System.out.print(DZ_shaxmatka.Task01.mas());
+       mas();
+    }
+    public static void mas(){
+        int [] mas = {0,1,0,1,0,1,0,1};
+    }
+    public static void mas1(){
+        
+    }
+}

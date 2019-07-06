da17da995b5a827e5f02d83eef4075db2bcdb848
Devastator1979 <devastator1979@mail.ru>
2019-07-06 14:28:59 +0300

    05

--- src/DZ_shaxmatka/Task01.java
+++ src/DZ_shaxmatka/Task01.java
@@ -1,25 +1,42 @@
 package DZ_shaxmatka;
-public class Task01 {
-    public static void main(String arg[]){
-       pmas(4);
-    }
-    public static void mas(){
-       int [] mas = {0,1,0,1,0,1,0,1};
-       int [] mas1 = {1,0,1,0,1,0,1,0};
-       for (int i = 0; i<mas.length ; i++){
-           System.out.print(mas[i] +"  ");
-       }
-       System.out.println();
-       for (int i = 0; i<mas1.length ; i++){
-           System.out.print(mas1[i] +"  ");
-       }
-       
-    }
-    public static void pmas(int a){
-        for (int i=0; i<a; i++){
-            mas();
-            System.out.println();
+
+class M {
+
+    int mas[][];
+    private Object DZ_shaxmatka;
+
+    public void init(int a) {
+        mas = new int[a][a];
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                if (i % 2 == 0) {
+                    mas[i][j] = 0;
+                } else {
+                    mas[i][j] = 1;
+                }
+            }
         }
-        
+        //for (int i = 0; i<mas.length ; i++){
+        //    i%2==0;
+        //    mas[i]=0;
+        //    
+        //}
+        //System.out.print(mas[i] +"  ");
     }
+
+    //System.out.println (mas[][]);
+    //for (int i = 0; i<mas1.length ; i++){
+    //    System.out.print(mas1[i] +"  ");
+    //}
+public void print(){
+       DZ_shaxmatka.M(int 10,int 10);
+}
+}
+
+public class Task01 {
+
+    public static void main(String arg[]) {
+        print();
+    }
+
 }

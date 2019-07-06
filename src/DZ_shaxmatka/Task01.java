882027ed31e7b594af58d9c9e9a0792bb087a52d
devas <devas@Devastator1979>
2019-07-06 12:21:19 +0300

    Еще добавил

--- src/DZ_shaxmatka/Task01.java
+++ src/DZ_shaxmatka/Task01.java
@@ -3,12 +3,20 @@ public class Task01 {
     public static void main(String arg[]){
        //System.out.println(Task01.mas.lenght);
        //System.out.print(DZ_shaxmatka.Task01.mas());
-       mas();
+       pmas(8);
     }
     public static void mas(){
-        int [] mas = {0,1,0,1,0,1,0,1};
+        int [] mas = {0,1,0,1,0,1,0,1};//new int [8];
+        //int mas[] = {0,1,0,1,0,1,0,1};
+       for (int i = 0; i<mas.length ; i++){
+           System.out.print(mas[i] +"  ");
+       }
     }
-    public static void mas1(){
+    public static void pmas(int a){
+        for (int i=0; i<a; i++){
+            mas();
+            System.out.println();
+        }
         
     }
 }
